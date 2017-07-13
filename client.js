448d13a11ee0980f2ed0331607cc7458f498537d
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2017-07-12 21:49:55 -0500

    Total refactor, add commands (quit, whisper)

--- client.js
+++ client.js
@@ -1,30 +1,51 @@
-var ansi = require('ansi-color');
+var color = require('ansi-color');
 var input = require('readline').createInterface(process.stdin, process.stdout);
+input.setPrompt(color.set('<me> ', 'green'));
 
 var socket = require('socket.io-client').connect('http://localhost:3636');
 out('Connected on 3636');
 
-var nick;
-
-input.question("Please enter a nickname: ", function(name) {
-    nick = name;
-    socket.emit('send', {type: 'notice', message: nick + " has joined the chat"});
+input.question('Please enter a nickname: ', function(name) {
+    socket.emit('nick', name);
     input.prompt(true);
 });
 
-input.on('line', function(line) {
-    if(line.length) {
-        socket.emit('send', {type: 'chat', message: line, nick: nick});
+input.on('line', function(data) {
+    if(data.length) {
+        if(data[0] == '/') {
+            var index = data.indexOf(' ');
+            var cmd = index == -1 ? data.substring(1) : data.substring(1, index);
+            switch(cmd) {
+                case 'quit':
+                    socket.disconnect();
+                    process.exit();
+                    break;
+                case 'w':
+                    var next = data.indexOf(' ', index + 1);
+                    socket.emit('whisper', {
+                        username: data.substring(index + 1, next),
+                        message: data.substring(next + 1)
+                    });
+                    break;
+            }
+        } else {
+            socket.emit('message', data);
+        }
+
         input.prompt(true);
     }
 });
 
 socket.on('message', function(data) {
-    if(data.type == "notice") {
-        out(color.set(data.message, 'cyan'));
-    } else if(data.nick != nick) {
-        out(color.set("<" + data.nick + "> ", "green") + data.message);
-    }
+    out(color.set('<' + data.username + '> ', 'green') + data.message);
+});
+
+socket.on('whisper', function(data) {
+    out(color.set('<' + data.username + '> ', 'yellow') + data.message);
+});
+
+socket.on('notice', function(data) {
+    out(color.set(data, 'cyan'));
 });
 
 function out(msg) {
--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 var io = require('socket.io').listen(3636);
 console.log('Listening on 3636');
 
+var users = {};
+
 io.sockets.on('connection', function(socket) {
-    socket.on('send', function(data) {
-        io.sockets.emit('message', data);
+    var added = false;
+
+    socket.on('nick', function(data) {
+        if(added) return;
+
+        added = true;
+        socket.username = data;
+        users[data] = socket;
+
+        socket.broadcast.emit('notice', socket.username + ' has joined');
+
+        socket.on('message', function(data) {
+            socket.broadcast.emit('message', {
+                username: socket.username,
+                message: data
+            });
+        });
+
+        socket.on('whisper', function(data) {
+            if(users.hasOwnProperty(data.username)) {
+                users[data.username].emit('whisper', {
+                    username: socket.username,
+                    message: data.message
+                });
+            }
+        });
+
+        socket.on('disconnect', function () {
+            socket.broadcast.emit('notice', socket.username + ' has left');
+        });
     });
 });
